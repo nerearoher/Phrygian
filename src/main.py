5d5ce009cb8fdb6b0a76e9ff0f28d40b6c640435
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2022-01-08 13:48:44 +0000

    Refactor melody extraction and parallelize it

--- src/main.py
+++ src/main.py
@@ -5,7 +5,25 @@
 
 import sys
 from glob import glob
-from music21 import converter, instrument, note, chord
+from multiprocessing import Pool
+from music21 import converter
+from music21.note import Note, Rest
+from music21.chord import Chord
+
+from melody import Melody
+
+def read_melodies_from_file(file: str):
+    print(f"Processing file: {file}")
+    score = converter.parse(file)
+    melodies = []
+    for part in score:
+        try
+            melody = Melody(part)
+        except:
+            continue
+        print(str(melody))
+        melodies.append(melody)
+    return melodies
 
 if len(sys.argv) != 2:
     print("Wrong number of arguments")
@@ -13,36 +31,7 @@ if len(sys.argv) != 2:
     exit()
 
 midi_files = glob(sys.argv[1] + "/**/*.mid")
-melodies = []
 
-for file in midi_files:
-    print(f"Processing file: {file}")
-    score = converter.parse(file)
-    for part in score:
-        melody = []
-        previous_pitch = None
-        for element in part.recurse():
-            print(element)
-            if element.quarterLength:
-                duration = element.quarterLength
-
-            if isinstance(element, note.Note):
-                if previous_pitch is None:
-                    melody.append((0, duration))
-                else:
-                    melody.append((element.pitch.midi - previous_pitch, duration))
-                previous_pitch = element.pitch.midi
-            elif isinstance(element, chord.Chord):
-                for pitch in element.pitches[:-1]:
-                    if previous_pitch is None:
-                        melody.append((0, duration))
-                    else:
-                        melody.append((pitch.midi - previous_pitch, 0))
-                    previous_pitch = pitch.midi
-                last_pitch = element.pitches[-1]
-                melody.append((last_pitch.midi - previous_pitch, duration))
-                previous_pitch = last_pitch.midi
-            elif isinstance(element, note.Rest) and len(melody) != 0:
-                melody[-1] = (melody[-1][0], melody[-1][1] + duration)
-        print(str(melody))
-        melodies.append(melody)
+pool = Pool()
+unflattened_melodies = pool.imap_unordered(read_melodies_from_file, midi_files)
+melodies = [m for melodies in unflattened_melodies for m in melodies]
---
+++ src/melody.py
@@ -0,0 +1,39 @@
+from music21.note import Note, Rest
+from music21.stream import Stream
+from music21.chord import Chord
+
+class Melody():
+    def __init__(self, part: Stream):
+        self.notes = []
+        self.previous_pitch = None
+        if part is not None: 
+            for element in part.recurse():
+                if isinstance(element, Note):
+                    self.add_note(element)
+                elif isinstance(element, Chord):
+                    self.add_chord(element)
+                elif isinstance(element, Rest):
+                    self.add_rest(element)
+
+    def add_note(self, note: Note):
+        duration = note.quarterLength
+        if self.previous_pitch is None:
+            self.notes.append((0, duration))
+        else:
+            self.notes.append((note.pitch.midi - self.previous_pitch, duration))
+        self.previous_pitch = note.pitch.midi
+
+    def add_chord(self, chord: Chord):
+        duration = chord.quarterLength
+        for pitch in chord.pitches[:-1]:
+            if self.previous_pitch is None:
+                self.notes.append((0, duration))
+            else:
+                self.notes.append((pitch.midi - self.previous_pitch, 0))
+            self.previous_pitch = pitch.midi
+        last_pitch = chord.pitches[-1]
+        self.notes.append((last_pitch.midi - self.previous_pitch, duration))
+
+    def add_rest(self, rest: Rest):
+        if len(self.notes) != 0:
+            self.notes[-1] = (self.notes[-1][0], self.notes[-1][1] + rest.quarterLength)
