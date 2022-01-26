980d321bd1707f62fffba4691a480b26f4648de9
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-01-26 20:47:43 +0000

    First and last note are the same

--- src/generate_music.py
+++ src/generate_music.py
@@ -17,22 +17,18 @@ def print_usage():
 def generate_notes(model, network_input, unique_pitches, unique_durations):
     base = sequences[np.random.randint(0, len(sequences) - 1)]
     base = base.tolist()
-    print(base)
 
     prediction_output = []
-    longest_duration = -1
-
-    while len(prediction_output) < 100 or prediction_output[-1][1] < longest_duration * 0.75 and len(prediction_output) < 300:
+    while len(prediction_output) < 100:
         input = np.reshape(base, (1, len(base), 1))
         prediction = model.predict(input, verbose=0)
 
         pitch = np.argmax(prediction[0][:len(unique_pitches)])
         duration = np.argmax(prediction[0][len(unique_durations):])
-        longest_duration = duration if duration > longest_duration else longest_duration
         prediction_output.append([unique_pitches[pitch], unique_durations[duration]])
 
-        base.insert(0, pitch)
-        base.insert(0, duration)
+        base.insert(0, unique_pitches[pitch])
+        base.insert(0, unique_durations[duration])
         base = base[:-2]
         if len(prediction_output) > 3:
             if prediction_output[-3][0] == prediction_output[-2][0] == prediction_output[-1][0]:
@@ -110,6 +106,12 @@ def generate_midi(initial_note, notes, name_instrument, name_scale):
         new_note = Note(pitch)
         new_note.quarterLength = note[1]
         stream.append(new_note)
+    
+    dif = (pitch - initial_pitch) % 12
+    pitch -= dif + (0 if dif > 6 else - 12)
+    new_note = Note(pitch)
+    new_note.quarterLength = 4
+    stream.append(new_note)
 
     stream = Stream(stream)
     stream.show()
@@ -145,4 +147,4 @@ with open(argv[1], "r") as input:
     notes = generate_notes(network, sequences, unique_pitches, unique_durations)
     midi = generate_midi(initial_note, notes, name_instrument, name_scale)
     midi.write('midi', fp=output_file)
-    print("Train network")
+    print("Music generated!")
