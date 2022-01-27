2ddf88f6eeaface0b958f7e308e0a1e8c9efb679
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2022-01-27 12:43:53 +0000

    Move train to the new CLI

--- src/phrygian.py
+++ src/phrygian.py
@@ -1,7 +1,7 @@
-
 from sys import argv
 from glob import glob
-
+from process_midis import process
+from train_network import train
 
 def print_process_usage():
     print("This subcommand processes a folder with midi files and produces a file with the results")
@@ -11,12 +11,22 @@ def process_cli(args):
     if len(args) != 2 or args[0] in ["-h", "--help"]:
         print_process_usage()
         exit()
+    process(*args)
 
+def print_train_usage():
+    print("This subcommand takes a preprocessed file and trains a neural network")
+    print("Usage: " + argv[0] + " <processed midis file> <neural network weights folder>")
+
+def train_cli(args):
+    if len(args) != 2 or args[0] in ["-h", "--help"]:
+        print_usage()
+        exit()
+    train(*args)
 
 commands = {
     "process": process_cli,
-    "train": "",
-    "generate": "",
+    "train": train_cli,
+    "generate": generate_cli,
 }
 
 def print_usage():
