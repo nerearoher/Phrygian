cbb1d5cd0358c79cf014fd447f7c8b10376effd1
J. Daniel Escánez <alu0101238944@ull.edu.es>
2022-01-30 02:33:40 +0000

    Functionality of phrygian in gui

--- src/phrygian_gui.py
+++ src/phrygian_gui.py
@@ -6,15 +6,24 @@ from PyQt5.QtWidgets import (
   QTextEdit, QComboBox, 
   QFormLayout, QLineEdit, 
   QGridLayout, QLineEdit,
-  QLayout, QSpinBox, QDialogButtonBox
+  QLayout, QSpinBox,
+  QDialogButtonBox, QMessageBox
 )
 from PyQt5.QtCore import Qt
 from css import PHRYGIAN_GUI
+from music21 import instrument
 
 # from process_midis import process
 # from train_network import train
 # from generate_music import generate
-from music21 import instrument
+
+# TODO: Eliminar esto cuando acabe el desarrollo de la gui
+def process(*args):
+  pass
+def train(*args):
+  pass
+def generate(*args):
+  pass
 
 PROCESS_STRING = 'Process MIDIs melodies'
 TRAIN_STRING = 'Train neuronal network'
@@ -159,6 +168,23 @@ SCALES = {
   "Enigma": [0, 1, 4, 6, 8, 10, 11, 12]
 }
 
+def accept_and_finish(args, function, dialog):
+  question = QMessageBox()
+  question.setText("The process is about to take place. Are you ready?")
+  question.setIcon(QMessageBox.Question)
+  question.setStandardButtons(QMessageBox.No | QMessageBox.Yes)
+  question.setDefaultButton(QMessageBox.Yes)
+  question.exec()
+
+  function(*args)
+
+  done = QMessageBox()
+  done.setText("Done!")
+  done.setIcon(QMessageBox.Information)
+  done.setStandardButtons(QMessageBox.Ok)
+  done.exec()
+  dialog.accept()
+
 def on_process_button_clicked():
   dialog = QDialog()
   dialog.setStyleSheet(PHRYGIAN_GUI)
@@ -169,40 +195,39 @@ def on_process_button_clicked():
   def set_midi_folder():
     directory_list = QFileDialog.getExistingDirectory(None, "Select MIDI input Directory")
     if directory_list:
-      line_edit_midi_folder.setText(directory_list[0])
+      line_edit_midi_folder.setText(directory_list)
 
   def set_output_file():
-    line_edit_output_file.setText(QFileDialog.getSaveFileName(None, 'Create a processed midi file', '', '')[0])
+    line_edit_output_file.setText(QFileDialog.getSaveFileName(None, 'Create a processed midi file (melodies)', '', '')[0])
 
   label_midi_folder = QLabel('Select a midi folder')
-  label_output_midi = QLabel('File to save the generated melodies')
   label_midi_folder.setObjectName('QLabelRow')
-  label_output_midi.setObjectName('QLabelRow')
-
-  browser_button_midi = QPushButton('Browser...')
-  browser_button_midi.setStyleSheet(PHRYGIAN_GUI)
-  browser_button_output = QPushButton('Browser...')
-  browser_button_output.setStyleSheet(PHRYGIAN_GUI)
-
   line_edit_midi_folder = QLineEdit()
   line_edit_midi_folder.setObjectName('QLineEdit')
   line_edit_midi_folder.setReadOnly(True)
+  browser_button_midi = QPushButton('Browser...')
+  browser_button_midi.setStyleSheet(PHRYGIAN_GUI)
   browser_button_midi.pressed.connect(set_midi_folder)
-  layout.addWidget(line_edit_midi_folder)
   layout.addRow(label_midi_folder, line_edit_midi_folder)
   layout.addWidget(browser_button_midi)
 
+  label_output_midi = QLabel('File to save the generated melodies')
+  label_output_midi.setObjectName('QLabelRow')
   line_edit_output_file = QLineEdit()
   line_edit_output_file.setObjectName('QLineEdit')
   line_edit_output_file.setReadOnly(True)
+  browser_button_output = QPushButton('Browser...')
+  browser_button_output.setStyleSheet(PHRYGIAN_GUI)
   browser_button_output.pressed.connect(set_output_file)
-  layout.addWidget(line_edit_output_file)
   layout.addRow(label_output_midi, line_edit_output_file)
   layout.addWidget(browser_button_output)
 
   qbtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
   buttonBox = QDialogButtonBox(qbtn)
-  buttonBox.accepted.connect(dialog.accept)
+  buttonBox.accepted.connect(lambda: accept_and_finish([
+    line_edit_midi_folder.text(),
+    line_edit_output_file.text()
+  ], process, dialog))
   buttonBox.rejected.connect(dialog.reject)
   layout.addWidget(buttonBox)
 
@@ -232,20 +257,21 @@ def on_train_button_clicked():
   line_edit_processed_midi = QLineEdit()
   line_edit_processed_midi.setReadOnly(True)
   browser_button_midi.pressed.connect(set_processed_midi)
-  layout.addWidget(line_edit_processed_midi)
   layout.addRow(label_processed_midi, line_edit_processed_midi)
   layout.addWidget(browser_button_midi)
 
   line_edit_weights_folder = QLineEdit()
   line_edit_weights_folder.setReadOnly(True)
   browser_button_wights.pressed.connect(set_weights_folder)
-  layout.addWidget(line_edit_weights_folder)
   layout.addRow(label_wights_folder, line_edit_weights_folder)
   layout.addWidget(browser_button_wights)
 
   qbtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
   buttonBox = QDialogButtonBox(qbtn)
-  buttonBox.accepted.connect(dialog.accept)
+  buttonBox.accepted.connect(lambda: accept_and_finish([
+    line_edit_processed_midi.text(),
+    line_edit_weights_folder.text()
+  ], train, dialog))
   buttonBox.rejected.connect(dialog.reject)
   layout.addWidget(buttonBox)
 
@@ -259,7 +285,7 @@ def on_generate_button_clicked():
   dialog.setWindowTitle(GENERATE_STRING)
 
   def set_processed_midi():
-    line_edit_processed_midi.setText(QFileDialog.getOpenFileName (None, 'Select processed midi file')[0])
+    line_edit_processed_midi.setText(QFileDialog.getOpenFileName (None, 'Select processed midi file (melodies)')[0])
 
   def set_weights_file():
     line_edit_weights.setText(QFileDialog.getOpenFileName(None, "Select neural networks weights")[0])
@@ -272,7 +298,6 @@ def on_generate_button_clicked():
   line_edit_processed_midi.setReadOnly(True)
   browser_button_midi = QPushButton('Browser...')
   browser_button_midi.pressed.connect(set_processed_midi)
-  layout.addWidget(line_edit_processed_midi)
   layout.addRow(label_processed_midi, line_edit_processed_midi)
   layout.addWidget(browser_button_midi)
 
@@ -281,7 +306,6 @@ def on_generate_button_clicked():
   line_edit_weights.setReadOnly(True)
   browser_button_nw = QPushButton('Browser...')
   browser_button_nw.pressed.connect(set_weights_file)
-  layout.addWidget(line_edit_weights)
   layout.addRow(label_weights, line_edit_weights)
   layout.addWidget(browser_button_nw)
 
@@ -290,7 +314,6 @@ def on_generate_button_clicked():
   line_edit_output_midi_file.setReadOnly(True)
   browser_button_output = QPushButton('Browser...')
   browser_button_output.pressed.connect(set_line_edit_output_midi_file)
-  layout.addWidget(line_edit_output_midi_file)
   layout.addRow(label_output_midi, line_edit_output_midi_file)
   layout.addWidget(browser_button_output)
 
@@ -304,7 +327,6 @@ def on_generate_button_clicked():
   spin_box_scale = QSpinBox()
   spin_box_scale.setRange(1, 8)
   spin_box_scale.setValue(3)
-  layout.addWidget(spin_box_scale)
 
   note_layout = QHBoxLayout()
   note_layout.addWidget(combo_box_pitch)
@@ -316,25 +338,30 @@ def on_generate_button_clicked():
   combo_box_instruments = QComboBox()
   combo_box_instruments.addItems(INSTRUMENTS.keys())
   combo_box_instruments.setCurrentIndex(list(INSTRUMENTS.keys()).index('Piano'))
-  layout.addWidget(combo_box_instruments)
   layout.addRow(label_instruments, combo_box_instruments)
 
   label_scale = QLabel('Select the scale')
   combo_box_scale = QComboBox()
   combo_box_scale.addItems(SCALES.keys())
-  layout.addWidget(combo_box_scale)
   layout.addRow(label_scale, combo_box_scale)
 
   label_number = QLabel('Select the number of notes')
   spin_box_notes = QSpinBox()
   spin_box_notes.setRange(2, 999999)
   spin_box_notes.setValue(100)
-  layout.addWidget(spin_box_notes)
   layout.addRow(label_number, spin_box_notes)
   
   qbtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
   buttonBox = QDialogButtonBox(qbtn)
-  buttonBox.accepted.connect(dialog.accept)
+  buttonBox.accepted.connect(lambda: accept_and_finish([
+    line_edit_processed_midi.text(),
+    line_edit_weights.text(),
+    line_edit_output_midi_file.text(),
+    str(combo_box_pitch.currentData()) + str(combo_box_accidental.currentData()) + str(spin_box_scale.value()), 
+    combo_box_instruments.currentData(),
+    combo_box_scale.currentData(),
+    spin_box_notes.value
+  ], generate, dialog))
   buttonBox.rejected.connect(dialog.reject)
   layout.addWidget(buttonBox)
 
